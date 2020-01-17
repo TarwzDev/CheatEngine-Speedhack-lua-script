0824cf741702a0c2181fed9729af59f4b35b32aa
arg9244 <arg9244@gmail.com>
2020-01-17 21:30:11 +0330

    Update SpeedHack.lua

--- SpeedHack.lua
+++ SpeedHack.lua
@@ -1,21 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<CheatTable CheatEngineTableVersion="29">
-  <CheatEntries>
-    <CheatEntry>
-      <ID>0</ID>
-      <Description>"PAGE UP Increase Speed"</Description>
-      <LastState Value="" RealAddress="00000000"/>
-      <GroupHeader>1</GroupHeader>
-    </CheatEntry>
-    <CheatEntry>
-      <ID>1</ID>
-      <Description>"PAGE DOWN Decrease Speed"</Description>
-      <LastState Value="" RealAddress="00000000"/>
-      <GroupHeader>1</GroupHeader>
-    </CheatEntry>
-  </CheatEntries>
-  <UserdefinedSymbols/>
-  <LuaScript>if myhotkey then
+if myhotkey then
   myhotkey.Destroy()
   myhotkey = nil
 end
@@ -35,5 +18,3 @@ speed = speed - 1
 speedhack_setSpeed(speed)
 end
 end, VK_NEXT) -- PAGE DOWN Decrease Speed
-</LuaScript>
-</CheatTable>
